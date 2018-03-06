fef15795dc141435874c1e360fe62ebee8f94c6f
Hrish1 <hmail17.95@gmail.com>
2018-03-06 18:01:25 +0530

    added custom api for local HIDs

---
+++ mininet/xia/xia.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+import sys
+from mininet.log import setLogLevel, info
+from mininet.cli import CLI
+from mininet.link import Link
+from net import Mininet_xia
+from node import Node_xia
+
+def topology():
+	"Create a network"
+
+	net = Mininet_xia(controller=None)
+
+	info("*** creating nodes\n")
+
+	xia1 = net.addXia('xia1', hid= 'a,b' )
+	xia2 = net.addXia('xia2' )
+	xia3 = net.addXia('xia3' )
+	net.addLink( xia1 , xia2 )
+	net.addLink( xia1 , xia3 )
+	if xia1 == None:
+		info("*** no node allocated\n")
+		return
+	
+	info("*** starting network \n")
+	net.build()
+
+	info("*** running cli\n")
+	CLI(net)
+
+	info("*** stopping network\n")
+	net.stop()
+	
+if __name__ == '__main__':
+	setLogLevel('info')
+	topology()
+
