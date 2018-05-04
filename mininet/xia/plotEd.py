e216691ae92cc3afe77c451aee92f11f18d4d061
Hrish1 <hmail17.95@gmail.com>
2018-05-05 00:37:47 +0530

    Added color for graphml nodes

--- mininet/xia/plotEd.py
+++ mininet/xia/plotEd.py
@@ -11,7 +11,13 @@ class TopologyVisualiser:
         self.buildElements()
 
     def setNode(self, node):
+
         Node = ET.Element("node", id=node)
+        data = ET.SubElement(Node, "data", key=node)
+        shapeNode = ET.SubElement(data, "y:ShapeNode")
+        Fill = ET.SubElement(shapeNode, "y:Fill")
+        Fill.set("color", "#FF0000")
+
         return Node
 
     def setLink(self, sourceIntf, targetIntf, linkId):
@@ -25,10 +31,18 @@ class TopologyVisualiser:
         return Link
 
     def buildElements(self):
+
         graphml = ET.Element("graphml", xmlns="http://graphml.graphdrawing.org/xmlns")
         graphml.set("xmlns:xsi", "http://www.w3.org/2001/XMLSchema-instance")
+        # graphml.set("xsi:schemaLocation",
+        #            "http://graphml.graphdrawing.org/xmlns http://graphml.graphdrawing.org/xmlns/1.0/graphml.xsd")
         graphml.set("xsi:schemaLocation",
-                    "http://graphml.graphdrawing.org/xmlns http://graphml.graphdrawing.org/xmlns/1.0/graphml.xsd")
+                    "http://graphml.graphdrawing.org/xmlns http://www.yworks.com/xml/schema/graphml/1.1/ygraphml.xsd")
+        graphml.set("xmlns:java", "http://www.yworks.com/xml/yfiles-common/1.0/java")
+        graphml.set("xmlns:sys", "http://www.yworks.com/xml/yfiles-common/markup/primitives/2.0")
+        graphml.set("xmlns:x", "http://www.yworks.com/xml/yfiles-common/markup/2.0")
+        graphml.set("xmlns:y", "http://www.yworks.com/xml/graphml")
+        graphml.set("xmlns:yed", "http://www.yworks.com/xml/yed/3")
 
         nodeKey = ET.SubElement(graphml, "key", id="d0")
         nodeKey.set("for", "node")
@@ -52,12 +66,12 @@ class TopologyVisualiser:
             Link = self.setLink(link.intf1, link.intf2, link.__str__())
             graph.append(Link)
 
-        #link = ET.SubElement(graph, "edge", id="e0")
-        #link.set("source", "n0")
-        #link.set("target", "n1")
-        #link = ET.SubElement(graph, "edge", id="e1")
-        #link.set("source", "n0")
-        #link.set("target", "n2")
+        # link = ET.SubElement(graph, "edge", id="e0")
+        # link.set("source", "n0")
+        # link.set("target", "n1")
+        # link = ET.SubElement(graph, "edge", id="e1")
+        # link.set("source", "n0")
+        # link.set("target", "n2")
 
         Tree = ET.ElementTree(graphml)
         filename = os.path.basename(sys.argv[0])
